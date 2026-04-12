d42e373bbc017e51c97a238d9662bde167f1756f
Dave Woodward <dave@woodwardmail.com>
2026-04-12 16:18:43 +0100

    Moved stats into the gpio module

--- bin/boilerswitch_core.py
+++ bin/boilerswitch_core.py
@@ -4,10 +4,9 @@ import time
 import logging
 import logging.handlers
 
-import gpio
-
 from alert import Alert
 from config import Config
+from gpio import Monitor
 
 def run_checker(config_file):
 
@@ -16,24 +15,6 @@ def run_checker(config_file):
     # Variables
     last_month = time.localtime().tm_mday  # Current Day of month as read on previous cycle.  Used to trigger Daily Stats reset.
     last_hour = time.localtime().tm_hour  # Current Hour as read on previous cycle.  Used to trigger logging of stats.
-
-    # Metrics measured from start of run.
-    run_stats = {
-        "reads": 0,         # Number of reads performed.
-        "clear": 0,         # Number of reads showing no fault.
-        "fault": 0,         # Number of reads showing a fault.
-        "misread": 0,       # Number of reads which appear to be misread.
-        "misread1st": 0,    # Number of misreads where first read is misread.
-    }
-
-    # Metrics measured from midnight.
-    daily_stats = {
-        "reads": 0,         # Number of reads performed.
-        "clear": 0,         # Number of reads showing no fault.
-        "fault": 0,         # Number of reads showing a fault.
-        "misread": 0,       # Number of reads which appear to be misread.
-        "misread1st": 0,    # Number of misreads where first read is misread.
-    }
 
     # Configure Logging
     my_logger = logging.getLogger("MyLogger")
@@ -46,23 +27,22 @@ def run_checker(config_file):
     log_format = '%(filename)s: %(levelname)s - %(message)s'
     handler.setFormatter(logging.Formatter(fmt=log_format))
 
-    gpio.init_board(cfg.gpio_pin)
+    monitor = Monitor(cfg.gpio_pin, my_logger)
     alerter = Alert(cfg)
 
     # Set Last Pin State to current Pin State
     # This avoids a mail being sent every time the program restarts. (Not normally a problem unless a error condition occurs where the
     # program constantly restarts)
-    last_pin_state = gpio.get_reading(cfg.gpio_pin, None, run_stats, daily_stats, my_logger)
+    last_pin_state = monitor.get_reading(None)
     my_logger.info(f"Program Starting.....")
     my_logger.info(f"-- with Pin State of        {last_pin_state}")
     my_logger.info(f"-- with timeframe of        {3600 / 3600} Hours") # TODO CONFIG
     my_logger.info(f"-- max emails per timeframe {14}") # TODO CONFIG
     my_logger.info(f"-- Time between checks      {cfg.sleep_time}   Seconds")
-
     my_logger.info(f"-- Recipients               {cfg.sendto}")
 
     while True:
-        pin_state = gpio.get_reading(cfg.gpio_pin, last_pin_state, run_stats, daily_stats, my_logger)
+        pin_state = monitor.get_reading(last_pin_state)
         month = time.localtime().tm_mday
         hour  = time.localtime().tm_hour
 
@@ -79,17 +59,10 @@ def run_checker(config_file):
                 alerter.send_fault()
 
         if last_hour != hour:
-            my_logger.info(f"STATS0: From Start    - Reads: {run_stats['reads']} - Clear: {run_stats['clear']} - Fault {run_stats['fault']} - Misread {run_stats['misread']} - Misread1st {run_stats['misread1st']}")
-            my_logger.info(f"STATS1: From Midnight - Reads: {daily_stats['reads']} - Clear: {daily_stats['clear']} - Fault {daily_stats['fault']} - Misread {daily_stats['misread']} - Misread1st {daily_stats['misread1st']}")
+            monitor.log_stats()
 
         if last_month != month:
-            daily_stats = {
-                "reads": 0,  # Number of reads performed.
-                "clear": 0,  # Number of reads showing no fault.
-                "fault": 0,  # Number of reads showing a fault.
-                "misread": 0,  # Number of reads which appear to be misread.
-                "misread1st": 0,  # Number of misreads where first read is misread.
-            }
+            monitor.reset_daily_stats()
 
         last_pin_state = pin_state
         last_hour = hour
--- bin/gpio.py
+++ bin/gpio.py
@@ -2,52 +2,91 @@ import time
 import logging
 import RPi.GPIO as GPIO
 
-def init_board(gpio_pin):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def get_reading(gpio_pin, last_pin_state, run_stats: dict, daily_stats: dict, my_logger: logging.Logger):
-    # In order to avoid false readings possibly due to cross talk...
-    # 4 readings are taken a short time apart, they all need to show a fault
-    # in order to read as a fault.
-    # global Reads, Clear, Fault, Misread, Misread1st
-    # global DailyReads, DailyClear, DailyFault, DailyMisread, DailyMisread1st
 
-    p1 = GPIO.input(gpio_pin)
-    time.sleep(0.05)
-    p2 = GPIO.input(gpio_pin)
-    time.sleep(0.05)
-    p3 = GPIO.input(gpio_pin)
-    time.sleep(0.05)
-    p4 = GPIO.input(gpio_pin)
+class Monitor:
+    def __init__(self, gpio_pin, my_logger: logging.Logger):
 
-    run_stats["reads"] += 1
-    daily_stats["reads"] += 1
+        self.gpio_pin = gpio_pin
+        self.logger = my_logger
+        # Metrics measured from start of run.
+        self.run_stats = {
+            "reads": 0,  # Number of reads performed.
+            "clear": 0,  # Number of reads showing no fault.
+            "fault": 0,  # Number of reads showing a fault.
+            "misread": 0,  # Number of reads which appear to be misread.
+            "misread1st": 0,  # Number of misreads where first read is misread.
+        }
 
-    true_reading = p1 | p2 | p3 | p4  # No Fault shows as 1 (Any read as 1 Causes no fault reading)
-    mis_reading_detect = p1 & p2 & p3 & p4  # Any read as 0 shows as fault, but this is used to detect misreads
-    # as all 4 reading should be the same.
+        # Metrics measured from midnight.
+        self.daily_stats = {
+            "reads": 0,  # Number of reads performed.
+            "clear": 0,  # Number of reads showing no fault.
+            "fault": 0,  # Number of reads showing a fault.
+            "misread": 0,  # Number of reads which appear to be misread.
+            "misread1st": 0,  # Number of misreads where first read is misread.
+        }
 
-    return_reading = true_reading
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    if true_reading and mis_reading_detect:  # Both types of measurements agree on no fault.
-        run_stats["clear"] += 1
-        daily_stats["clear"] += 1
+    def get_reading(self, last_pin_state):
+        # In order to avoid false readings possibly due to cross talk...
+        # 4 readings are taken a short time apart, they all need to show a fault
+        # in order to read as a fault.
+        # global Reads, Clear, Fault, Misread, Misread1st
+        # global DailyReads, DailyClear, DailyFault, DailyMisread, DailyMisread1st
 
-    elif true_reading != mis_reading_detect:  # Both types of measurements disagree on no fault.
-        run_stats["misread"] += 1
-        daily_stats["misread"] += 1
+        p1 = GPIO.input(self.gpio_pin)
+        time.sleep(0.05)
+        p2 = GPIO.input(self.gpio_pin)
+        time.sleep(0.05)
+        p3 = GPIO.input(self.gpio_pin)
+        time.sleep(0.05)
+        p4 = GPIO.input(self.gpio_pin)
 
-        if p1 == 0:
-            run_stats["misread1st"] += 1
-            daily_stats["misread1st"] += 1
+        self.run_stats["reads"] += 1
+        self.daily_stats["reads"] += 1
 
-        return_reading = last_pin_state  # A misread should not be detected as a no alarm, as misreads can happen (rarely) when circuit closed.
-        my_logger.debug(f"{p1} {p2} {p3} {p4} -- MISREAD")
+        true_reading = p1 | p2 | p3 | p4  # No Fault shows as 1 (Any read as 1 Causes no fault reading)
+        mis_reading_detect = p1 & p2 & p3 & p4  # Any read as 0 shows as fault, but this is used to detect misreads
+        # as all 4 reading should be the same.
 
-    else:  # Otherwise must be true fault (p1 -> p4 show 0)
-        run_stats["fault"] += 1
-        daily_stats["fault"] += 1
-        # my_logger.debug(f"{p1} {p2} {p3} {p4} -- FAULT")
+        return_reading = true_reading
 
-    return return_reading
+        if true_reading and mis_reading_detect:  # Both types of measurements agree on no fault.
+            self.run_stats["clear"] += 1
+            self.daily_stats["clear"] += 1
+
+        elif true_reading != mis_reading_detect:  # Both types of measurements disagree on no fault.
+            self.run_stats["misread"] += 1
+            self.daily_stats["misread"] += 1
+
+            if p1 == 0:
+                self.run_stats["misread1st"] += 1
+                self.daily_stats["misread1st"] += 1
+
+            return_reading = last_pin_state  # A misread should not be detected as a no alarm, as misreads can happen (rarely) when circuit closed.
+            self.logger.debug(f"{p1} {p2} {p3} {p4} -- MISREAD")
+
+        else:  # Otherwise must be true fault (p1 -> p4 show 0)
+            self.run_stats["fault"] += 1
+            self.daily_stats["fault"] += 1
+            # self.logger.debug(f"{p1} {p2} {p3} {p4} -- FAULT")
+
+        return return_reading
+
+    def log_stats(self):
+        self.logger.info(
+            f"STATS0: From Start    - Reads: {self.run_stats['reads']} - Clear: {self.run_stats['clear']} - Fault {self.run_stats['fault']} - Misread {self.run_stats['misread']} - Misread1st {self.run_stats['misread1st']}")
+        self.logger.info(
+            f"STATS1: From Midnight - Reads: {self.daily_stats['reads']} - Clear: {self.daily_stats['clear']} - Fault {self.daily_stats['fault']} - Misread {self.daily_stats['misread']} - Misread1st {self.daily_stats['misread1st']}")
+
+    def reset_daily_stats(self):
+        self.daily_stats = {
+            "reads": 0,  # Number of reads performed.
+            "clear": 0,  # Number of reads showing no fault.
+            "fault": 0,  # Number of reads showing a fault.
+            "misread": 0,  # Number of reads which appear to be misread.
+            "misread1st": 0,  # Number of misreads where first read is misread.
+        }
